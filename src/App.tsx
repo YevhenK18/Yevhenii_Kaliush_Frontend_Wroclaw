a569bd493e937c36d319369bb43880e35dc040fe
YevhenK18 <yevheniikaliush@gmail.com>
2025-05-09 17:49:17 +0200

    Fix Router conflict by removing duplicate BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-     import ProductList from './components/ProductList';
-     import Cart from './components/Cart';
-     import OrderSummary from './components/OrderSummary';
-     import OrderConfirmation from './components/OrderConfirmation';
+import { Routes, Route } from 'react-router-dom';
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
+import OrderSummary from './components/OrderSummary';
+import OrderConfirmation from './components/OrderConfirmation';
+import './styles.css';
 
-     function App() {
-       return (
-         <Router basename="/Yevhenii_Kaliush_Frontend_Wroclaw">
-           <Routes>
-             <Route path="/" element={<ProductList />} />
-             <Route path="/cart" element={<Cart />} />
-             <Route path="/order-summary" element={<OrderSummary />} />
-             <Route path="/order-confirmation" element={<OrderConfirmation />} />
-           </Routes>
-         </Router>
-       );
-     }
+function App() {
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order-summary" element={<OrderSummary />} />
+        <Route path="/order-confirmation" element={<OrderConfirmation />} />
+      </Routes>
+    </div>
+  );
+}
 
-     export default App;
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-     import ReactDOM from 'react-dom/client';
-     import './index.css';
-     import App from './App';
-     import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-     const redirectPath = sessionStorage.getItem('redirectPath');
-     if (redirectPath && redirectPath !== '/' && window.location.pathname === '/') {
-       sessionStorage.removeItem('redirectPath');
-       window.history.replaceState(null, '', redirectPath);
-     }
+const redirectPath = sessionStorage.getItem('redirectPath');
+if (redirectPath && redirectPath !== '/' && window.location.pathname === '/') {
+  sessionStorage.removeItem('redirectPath');
+  window.history.replaceState(null, '', redirectPath);
+}
 
-     const root = ReactDOM.createRoot(
-       document.getElementById('root') as HTMLElement
-     );
-     root.render(
-       <React.StrictMode>
-         <BrowserRouter basename="/yevhenii_kaliush_frontend_wroclaw">
-           <App />
-         </BrowserRouter>
-       </React.StrictMode>
-     );
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter basename="/Yevhenii_Kaliush_Frontend_Wroclaw">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
