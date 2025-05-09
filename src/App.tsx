84e7021c0c695b8a74e71cadd4297f7e042c625d
YevhenK18 <yevheniikaliush@gmail.com>
2025-05-09 17:21:18 +0200

    v.1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ProductList from './components/ProductList';
-import Cart from './components/Cart';
-import OrderSummary from './components/OrderSummary';
-import OrderConfirmation from './components/OrderConfirmation';
+     import ProductList from './components/ProductList';
+     import Cart from './components/Cart';
+     import OrderSummary from './components/OrderSummary';
+     import OrderConfirmation from './components/OrderConfirmation';
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<ProductList />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/order-summary" element={<OrderSummary />} />
-        <Route path="/order-confirmation" element={<OrderConfirmation />} />
-      </Routes>
-    </Router>
-  );
-}
+     function App() {
+       return (
+         <Router basename="/Yevhenii_Kaliush_Frontend_Wroclaw">
+           <Routes>
+             <Route path="/" element={<ProductList />} />
+             <Route path="/cart" element={<Cart />} />
+             <Route path="/order-summary" element={<OrderSummary />} />
+             <Route path="/order-confirmation" element={<OrderConfirmation />} />
+           </Routes>
+         </Router>
+       );
+     }
 
-export default App;
+     export default App;
